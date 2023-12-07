f4de25ffdfd29dcd1149585ee9bf0a80d12bfe39
DanielAglr <81238732+DanielAglr@users.noreply.github.com>
2023-12-07 12:48:23 -0800

    Define basic token types and token structure

---
+++ include/tokens.h
@@ -0,0 +1,31 @@
+#ifndef TOKENS_H
+#define TOKENS_H
+
+#include <string>
+
+enum TokenType {
+  // Single-Character Tokens
+  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACK, RIGHT_BRACK, SEMICOLON,
+
+  // Literals
+  IDENTIFIER, STRING, NUMBER,
+
+  // Keywords
+  PRINT,
+
+  EoF
+};
+
+struct Token {
+  TokenType type;
+  std::string lexeme;
+  int line;
+
+  Token(TokenType type, std::string lexeme, int line) : type(type), lexeme(lexeme), line(line) {};
+
+  inline std::string toString() { 
+    return type + " " + lexeme; 
+  }
+};
+
+#endif
